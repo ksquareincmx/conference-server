69ff41b3c557e96fde51b1a98c09d9feb48e8d63
JosselOzoryu <jose.osorno@ksquareinc.com>
2018-09-20 16:58:03 -0500

    Initial route implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import LoginPage from "./pages/Login";
+import Routes from "../../routes/routes";
 import { AuthProvider, AuthConsumer } from "./providers/Auth";
 
 class App extends Component {
   render() {
     return (
       <AuthProvider>
-        <AuthConsumer>{auth => <LoginPage auth={auth} />}</AuthConsumer>
+        <Routes>
+          <AuthConsumer>{auth => <LoginPage auth={auth} />}</AuthConsumer>
+        </Routes>
       </AuthProvider>
     );
   }
---
+++ routes/routes.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Routes = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/calendar" component={Calendar} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Login = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Topics</h2>
+  </div>
+);
+
+const Calendar = () => (
+  <div>
+    <h3>Calendar</h3>
+  </div>
+);
+
+export default Routes;
