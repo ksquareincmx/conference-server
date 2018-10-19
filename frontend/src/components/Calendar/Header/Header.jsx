997019a6ad5df96567920ccfbd573298cc3bc082
Victor Burgos <victor.burgos@ksquareinc.com>
2018-10-19 10:32:24 -0500

    Implemented labels for each specific view

--- frontend/src/components/Calendar/Header/Header.jsx
+++ frontend/src/components/Calendar/Header/Header.jsx
@@ -1,67 +1,10 @@
 import React from 'react';
 import './Header.css';
 
-const daysNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const monthsNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-const weekOfYear = date => {
-  let d = new Date(+date);
-  d.setHours(0, 0, 0);
-  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-  return Math.ceil(((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7 + 1) / 7);
-};
-
-const headerDateContainer = (date, typeView) => {
-  switch (typeView) {
-    case 'day':
-      return (
-        <div className="header-date-container">
-          <p>{daysNames[date.getDay()]}</p>
-          <p>{`${monthsNames[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`}</p>
-        </div>
-      );
-    case 'work_week':
-      return (
-        <div className="header-date-container">
-          <p>Week #{weekOfYear(date)}</p>
-          <p>{`${monthsNames[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`}</p>
-        </div>
-      );
-    case 'month':
-      return (
-        <div className="header-date-container">
-          <p>{monthsNames[date.getMonth()]}</p>
-          <p>{date.getFullYear()}</p>
-        </div>
-      );
-    case 'year':
-      return (
-        <div className="header-date-container">
-          <p>{date.getFullYear()}</p>
-        </div>
-      );
-    default:
-      return {};
-  }
-};
-
 const HeaderView = props => {
   return (
     <div className="header-container">
-      {headerDateContainer(props.date, props.type)}
+      {props.headerDateContainer}
       <div className="header-view-selector">
         <br />
         <div className="veiew-buttons-container">
--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -12,12 +12,68 @@ import dateFormat from 'dateformat';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './Calendar.css';
 
+const dateToday = new Date();
+
+// Constants for HeaderStrategy
+const daysNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthsNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const dayNameToday = daysNames[dateToday.getDay()];
+const monthNameToday = monthsNames[dateToday.getMonth()];
+
+// Constants for CalendarStrategy
 const localizer = BigCalendar.momentLocalizer(moment);
 const minDate = dates.add(dates.startOf(new Date(), 'day'), -16, 'hours');
 const maxDate = dates.add(dates.endOf(new Date(), 'day'), -5, 'hours');
-const dateToday = new Date();
 const step = 15;
 const timeSlots = 4;
+
+const HeaderStrategy = props => {
+  switch (props.type) {
+    case 'day':
+      return (
+        <div className="header-date-container">
+          <p>{props.dayName}</p>
+          <p>{`${props.monthName} ${props.numberDayInMonth} ${props.fullYear}`}</p>
+        </div>
+      );
+    case 'work_week':
+      return (
+        <div className="header-date-container">
+          <p>Week #{props.numberWeekInYear}</p>
+          <p>{`${props.monthName} ${props.numberDayInMonth} ${props.fullYear}`}</p>
+        </div>
+      );
+    case 'month':
+      return (
+        <div className="header-date-container">
+          <p>{props.monthName}</p>
+          <p>{props.fullYear}</p>
+        </div>
+      );
+    case 'year':
+      return (
+        <div className="header-date-container">
+          <br />
+          <p>{props.fullYear}</p>
+        </div>
+      );
+    default:
+      return {};
+  }
+};
 
 const CalendarStrategy = props => {
   switch (props.type) {
@@ -87,10 +143,61 @@ class CalendarPage extends React.Component {
     console.log(buttonId);
   };
 
+  getWeekOfYear = date => {
+    let d = new Date(+date);
+    d.setHours(0, 0, 0);
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    return Math.ceil(((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7 + 1) / 7);
+  };
+
+  FooterChangeButtonLabels = type => {
+    switch (type) {
+      case 'day':
+        return {
+          previousButtonLabel: 'Previus day',
+          nextButtonLabel: 'Next day',
+        };
+
+      case 'work_week':
+        return {
+          previousButtonLabel: 'Previus week',
+          nextButtonLabel: 'Next week',
+        };
+      case 'month':
+        return {
+          previousButtonLabel: 'Previus month',
+          nextButtonLabel: 'Next month',
+        };
+      case 'year':
+        return {
+          previousButtonLabel: 'Previus year',
+          nextButtonLabel: 'Next year',
+        };
+      default:
+        return {
+          previousButtonLabel: '',
+          nextButtonLabel: '',
+        };
+    }
+  };
+
   render() {
     return (
       <div className="calendar-container">
-        <HeaderView onClickViewButton={this.handlerOnClickViewButton} date={dateToday} type={this.state.selector} />
+        <HeaderView
+          onClickViewButton={this.handlerOnClickViewButton}
+          headerDateContainer={
+            <HeaderStrategy
+              type={this.state.selector}
+              dayName={dayNameToday}
+              monthName={monthNameToday}
+              numberDayInMonth={dateToday.getDate()}
+              numberWeekInYear={this.getWeekOfYear(dateToday)}
+              fullYear={dateToday.getFullYear()}
+              date={dateToday}
+            />
+          }
+        />
         <CalendarStrategy
           type={this.state.selector}
           events={this.state.events}
@@ -105,7 +212,12 @@ class CalendarPage extends React.Component {
           timeSlots={timeSlots}
           date={dateToday}
         />
-        <FooterView onClickButton={this.handlerOnCLickTimeButton} date={dateToday} type={this.state.selector} />
+        <FooterView
+          {...this.FooterChangeButtonLabels(this.state.selector)}
+          onClickButton={this.handlerOnCLickTimeButton}
+          date={dateToday}
+          type={this.state.selector}
+        />
       </div>
     );
   }
