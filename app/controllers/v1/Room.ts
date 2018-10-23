f334cdcfa299bdc2b156556c0a25d58ad79e38d6
Miguel <miguel.delgado@ksquareinc.com>
2018-10-23 10:21:46 -0500

    Added room status

--- app/controllers/v1/Room.ts
+++ app/controllers/v1/Room.ts
@@ -1,11 +1,18 @@
-
+import { Op } from "sequelize";
 import { Controller } from "./../../libraries/Controller";
+import { getActualDate } from "./../../libraries/util";
 import { Room } from "./../../models/Room";
+import { Booking } from "./../../models/Booking";
 import { Request, Response, Router } from "express";
-import { validateJWT, filterOwner, appendUser, stripNestedObjects, filterRoles } from "./../../policies/General";
+import {
+  validateJWT,
+  filterOwner,
+  appendUser,
+  stripNestedObjects,
+  filterRoles
+} from "./../../policies/General";
 
 export class RoomController extends Controller {
-
   constructor() {
     super();
     this.name = "room";
@@ -13,11 +20,10 @@ export class RoomController extends Controller {
   }
 
   routes(): Router {
-
     /**
         @api {get} /api/v1/Room/ Get a list of Rooms
+          @apiName getAllRooms
         @apiPermission access
-        @apiName getAllRooms
         @apiGroup Room
 
         @apiHeader { String }   Content-Type Application/Json
@@ -26,13 +32,16 @@ export class RoomController extends Controller {
         @apiSuccess  {Object[]}   body                    Room details
         @apiSuccess  {String}     body.name               Room name
         @apiSuccess  {String}     body.color              The color to show in the UI for this room
-        @apiSuccess  {Boolean}     body.presence          If there is someone in the room
+        @apiSuccess  {Boolean}    body.presence           If there is someone in the room (For future sensor integration)
+        @apiSuccess  {Number}     body.bookingId          Booking id if not available, null if available
+        @apiSuccess  {Date}        body.updatedAt         Room creation date
+        @apiSuccess  {Date}        body.createdAt         Room update date
+
+
     */
 
-    this.router.get(
-      "/",
-      validateJWT("access"),
-      (req, res) => this.find(req, res)
+    this.router.get("/", validateJWT("access"), (req, res) =>
+      this.findAllRoom(req, res)
     );
 
     /**
@@ -48,12 +57,14 @@ export class RoomController extends Controller {
         @apiSuccess  {String}   body.name           Room name
         @apiSuccess  {String}   body.color          The color to show in the UI for this room
         @apiSuccess  {Boolean}  body.presence       If there is someone in the room
+        @apiSuccess  {Number}   body.bookingId      Booking id if not available, null if available
+        @apiSuccess  {Date}     body.updatedAt      Room creation date
+        @apiSuccess  {Date}     body.createdAt      Room update date
+
     */
 
-    this.router.get(
-      "/:id",
-      validateJWT("access"),
-      (req, res) => this.findOne(req, res)
+    this.router.get("/:id", validateJWT("access"), (req, res) =>
+      this.findOneRoom(req, res)
     );
 
     /**
@@ -71,17 +82,21 @@ export class RoomController extends Controller {
         @apiParam    {Boolean}  body.presence       If there is someone in the room
 
         @apiSuccess  {Object}   body                Room details
+        @apiSuccess  {Number}   body.id             Room id
         @apiSuccess  {String}   body.name           Room name
         @apiSuccess  {String}   body.color          The color to show in the UI for this room
         @apiSuccess  {Boolean}  body.presence       If there is someone in the room
+        @apiSuccess  {Date}     body.updatedAt      Room creation date
+        @apiSuccess  {Date}     body.createdAt      Room update date
+
     */
 
     this.router.post(
       "/",
       validateJWT("access"),
       stripNestedObjects(),
-      filterRoles(["admin"]),
-        (req, res) => this.create(req, res)
+      //filterRoles(["admin"]),
+      (req, res) => this.create(req, res)
     );
 
     /**
@@ -102,6 +117,9 @@ export class RoomController extends Controller {
         @apiSuccess  {String}   body.name           Room name
         @apiSuccess  {String}   body.color          The color to show in the UI for this room
         @apiSuccess  {Boolean}  body.presence       If there is someone in the room
+        @apiSuccess  {Date}     body.updatedAt      Room creation date
+        @apiSuccess  {Date}     body.createdAt      Room update date
+
     */
 
     this.router.put(
@@ -133,6 +151,93 @@ export class RoomController extends Controller {
     return this.router;
   }
 
+  findOneRoom(req: Request, res: Response) {
+    let roomId = req.params.id;
+    let actualDate = getActualDate();
+
+    this.model
+      .findById(roomId)
+      .then(async roomResult => {
+        if (!roomResult) res.status(404).end();
+        else {
+          let room = JSON.parse(JSON.stringify(roomResult, null, 2));
+
+          let bookingResult = await Booking.findOne({
+            attributes: ["id"],
+            where: {
+              [Op.and]: {
+                roomId: {
+                  [Op.eq]: roomId
+                },
+                start: {
+                  [Op.lte]: actualDate
+                },
+                end: {
+                  [Op.gte]: actualDate
+                }
+              }
+            }
+          });
+          let parsedBooking = JSON.parse(
+            JSON.stringify(bookingResult, null, 2)
+          );
+          if (parsedBooking) {
+            room["bookingId"] = bookingResult.id;
+          } else {
+            room["bookingId"] = null;
+          }
+
+          res.status(200).json(room);
+        }
+        return null;
+      })
+      .catch(err => {
+        if (err) Controller.serverError(res, err);
+      });
+  }
+
+  findAllRoom(req: Request, res: Response) {
+    let actualDate = getActualDate();
+
+    this.model
+      .findAll()
+      .then(async resultRooms => {
+        let parsedRooms = JSON.parse(JSON.stringify(resultRooms, null, 2));
+
+        let rooms = parsedRooms.map(async room => {
+          let bookingResult = await Booking.findOne({
+            attributes: ["id"],
+            where: {
+              [Op.and]: {
+                roomId: {
+                  [Op.eq]: room.id
+                },
+                start: {
+                  [Op.lte]: actualDate
+                },
+                end: {
+                  [Op.gte]: actualDate
+                }
+              }
+            }
+          });
+          let parsedBookings = JSON.parse(
+            JSON.stringify(bookingResult, null, 2)
+          );
+          if (parsedBookings) {
+            room["bookingId"] = bookingResult.id;
+          } else {
+            room["bookingId"] = null;
+          }
+          return room;
+        });
+        rooms = await Promise.all(rooms);
+        res.status(200).json(rooms);
+      })
+      .catch(err => {
+        return Controller.serverError(err);
+      });
+  }
 }
 
 const room = new RoomController();
