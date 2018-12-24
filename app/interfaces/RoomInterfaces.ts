e4c1121e81dd30df99ba3d239abf77fb5418a2a4
Miguel Delgado <mdelga.ac@gmail.com>
2018-12-24 11:41:21 -0600

    Added Room interfaces

---
+++ app/interfaces/RoomInterfaces.ts
@@ -0,0 +1,29 @@
+export interface IRoomResponse {
+  id: number;
+  name: string;
+  color: string;
+  presence: boolean;
+  created_at: Date;
+  updated_at: Date;
+  booking_id_actual: number;
+  status: string;
+}
+
+export interface IFindRoom {
+  id: number;
+}
+
+export interface IFindAllRoom {}
+
+export interface IDeleteRoom {
+  id: number;
+}
+
+export interface IRoomRequest {
+  name: string;
+  color: string;
+}
+
+export interface ICreateRoom extends IRoomRequest {}
+
+export interface IUpdateRoom extends IRoomRequest {}
